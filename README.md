41a994c2968b45f775c579723f6e5ed393ed8c93
heyjoakim <joakimhey@gmail.com>
2020-04-27 16:11:36 +0200

    updaded app

--- README.md
+++
@@ -1,1 +0,0 @@
-# RTCS-lang
--- src/App/obj/Debug/netcoreapp3.1/App.AssemblyInfo.fs
+++ src/App/obj/Debug/netcoreapp3.1/App.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("RTCS-lang")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("App")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
 [<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("RTCS-lang")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("RTCS-lang")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("App")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("App")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
