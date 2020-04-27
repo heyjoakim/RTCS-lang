834c1ca463673383eb7fe1dd2c0793122d879ba2
heyjoakim <joakimhey@gmail.com>
2020-04-27 16:14:10 +0200

    patterns added from parser to eval

--- src/App/Lang.fs
+++ src/App/Lang.fs
@@ -1,7 +1,6 @@
 module Lang
 
 open Parser
-open System
 
 // Interpreter
 type 'a env = (varname list * 'a) list
@@ -23,3 +22,19 @@ let rec eval env =
         eval ((x, v1) :: env) e2
     | EQ(e1, e2) ->
         if eval env e1 = eval env e2 then 1 else 0
+    | SUB(_, _) -> failwith "Not Implemented"
+    | NEG(_) -> failwith "Not Implemented"
+    | MUL(_, _) -> failwith "Not Implemented"
+    | DIV(_, _) -> failwith "Not Implemented"
+    | NEQ(_, _) -> failwith "Not Implemented"
+    | LT(_, _) -> failwith "Not Implemented"
+    | LE(_, _) -> failwith "Not Implemented"
+    | GT(_, _) -> failwith "Not Implemented"
+    | GE(_, _) -> failwith "Not Implemented"
+    | IF(_, _, _) -> failwith "Not Implemented"
+    | AND(_, _) -> failwith "Not Implemented"
+    | OR(_, _) -> failwith "Not Implemented"
+    | CALL(_, _) -> failwith "Not Implemented"
+    | FAIL -> failwith "Not Implemented"
+    | CATCH(_, _) -> failwith "Not Implemented"
+    | SEQ(_, _) -> failwith "Not Implemented"
